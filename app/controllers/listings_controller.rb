b04411f07eee6ff12ac3f04d0f24d3b7bc5cb5fe
Adam Jubert <adamjubert@Adams-MacBook-Air.local>
2025-10-18 13:11:45 -0400

    Add listings model, ux/ui, tailwind

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,29 @@
+class ListingsController < ApplicationController
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+
+    if @listing.save
+      redirect_to @listing
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:body)
+  end
+end
---
+++ test/models/listing_summary_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ListingSummaryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
