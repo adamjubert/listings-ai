6e83a221f6da45c797688baf884cb049649cf26e
Adam <jubert.adam@gmail.com>
2025-10-19 12:15:03 -0400

    Show page updates

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,4 +5,32 @@ class Listing < ApplicationRecord
     return nil if summary.nil?
     summary.map { |k, v| "#{k}: #{v}" }.join("\n")
   end 
+
+  def day
+    summary&.dig('day')
+  end
+
+  def time
+    summary&.dig('time')
+  end
+
+  def price
+    summary&.dig('price')
+  end
+
+  def title
+    summary&.dig('title')
+  end
+
+  def location
+    summary&.dig('location')
+  end
+
+  def categories
+    summary&.dig('categories')
+  end
+
+  def description
+    summary&.dig('description')
+  end
 end
